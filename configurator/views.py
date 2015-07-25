f3aa5f9e37751d9d4c76a979d2e001627c95f5c0
mdaif (mohamed.daif) <muhammad.daif@gmail.com>
2015-07-26 01:46:37 +0200

    sends data from frontend to backend, adds dummy implementation for the backend handler

--- configurator/views.py
+++ configurator/views.py
@@ -1,7 +1,10 @@
 from django.views.generic.base import TemplateView
+from django.views.generic import View
 from IPython.parallel import Client
+from django.http import HttpResponse
+import json
 
-# Create your views here.
+
 class HomePageView(TemplateView):
 
     template_name = "home.html"
@@ -13,6 +16,13 @@ class HomePageView(TemplateView):
             context['workers_count'] = len(rc.ids)
         except:
             pass
-
-
         return context
+
+
+class CommandExecutionView(View):
+    def post(self, request, *args, **kwargs):
+        print("**************")
+        print(request.POST)
+        print("**************")
+        return HttpResponse(json.dumps({'success': True}), content_type='application/json', status=200)
+
--- knob/urls.py
+++ knob/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from configurator.views import HomePageView
+from configurator.views import HomePageView, CommandExecutionView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', HomePageView.as_view(), name='home')
+    url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'^commands/execute/?$', CommandExecutionView.as_view(), name='exec_command')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
