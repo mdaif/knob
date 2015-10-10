4c64fa621b4e699f5f03b9c4afab8440ffcc7c73
mdaif (mohamed.daif) <muhammad.daif@gmail.com>
2015-10-10 11:31:23 +0200

    seperates knob urls, moves python shell button to be above commands text area

--- knob/urls.py
+++ knob/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from configurator.views import HomePageView, CommandExecutionView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', HomePageView.as_view(), name='home'),
-    url(r'^commands/execute/?$', CommandExecutionView.as_view(), name='exec_command')
+    url(r'^knob/', include('configurator.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
